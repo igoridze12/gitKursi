5a0bd6940366e159f583545e036a6331af6a3ac9
igoridze12 <igor73rus12@gmail.com>
2022-04-14 23:48:36 +0300

    задачи с циклом

---
+++ hui.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+void main(List<String> args) {
+var n = 0;
+while (n < 20){
+  n++;
+if (n % 3 ==0 ){
+  print('Fizz');
+}
+ else if (n % 5 ==0){
+   print('Buzz');
+ }
+ else if (n % 3 == 0 && n % 5 ==0 ){
+   print('FizzBuzz');
+ }
+ else {
+  print(n);
+}
+}
+}
