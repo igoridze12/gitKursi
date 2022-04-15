020432d3c7ba838ae6932008dda783c5803d4b47
igoridze12 <igor73rus12@gmail.com>
2022-04-15 16:18:40 +0300

    zadachka

--- hui.dart
+++ hui.dart
@@ -1,19 +1,15 @@
 import 'dart:io';
 void main(List<String> args) {
-var n = 0;
-while (n < 20){
-  n++;
-if (n % 3 ==0 ){
-  print('Fizz');
-}
- else if (n % 5 ==0){
-   print('Buzz');
- }
- else if (n % 3 == 0 && n % 5 ==0 ){
-   print('FizzBuzz');
- }
- else {
-  print(n);
-}
+var a = 12;
+var b = 16;
+  while (a!=0 && b!=0){
+   if (a>b){
+     a %=b;
+   }
+   else {
+     b %=a;
+   }
+   print(a+b);
+   
 }
 }
